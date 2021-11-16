661f58ce18fc9f0340e0ea488a845b4af9f66a8b
mellabanca <91677889+mellabanca@users.noreply.github.com>
2021-11-16 10:11:33 -0300

    Arquivos para o dia 16-11

--- sketch.js
+++ sketch.js
@@ -1,30 +1,29 @@
 var Trex, TrexCorrendo;
-
-
+var chao, chaoimagem;
 function preload(){
   TrexCorrendo = loadAnimation("trex1.png","trex3.png","trex4.png");
-
+  chaoimagem = loadImage ("ground2.png");
 }
-
 function setup(){
-  createCanvas(600,200)
-  Trex = createSprite(50,160,20,50);
-  Trex.addAnimation("correndo",TrexCorrendo);
-  Trex.scale = 0.5;
-  borda = createEdgeSprites();
+createCanvas(600,200)
+Trex = createSprite(50,160,20,50);
+Trex.addAnimation("correndo",TrexCorrendo);
+Trex.scale = 0.5;
+borda = createEdgeSprites();
+chao = createSprite(200,180,400,20);
+chao.addImage("chao",chaoimagem);
+chao.x = chao.width/2;
 }
-
 function draw(){
-  background("white");
-
-  if(keyDown("space")){
-    Trex.velocityY = -10;
-  }
-
-  Trex.velocityY = Trex.velocityY + 1;
-
-  Trex.collide(borda[3]);
-
-  drawSprites();
-
+background("white");
+chao.velocityX = -2;
+if(keyDown("space")){
+Trex.velocityY = -10;
+}
+if (chao.x < 0){
+chao.x = chao.width/2;
+}
+Trex.velocityY = Trex.velocityY + 1;
+Trex.collide(chao);
+drawSprites();
 }
